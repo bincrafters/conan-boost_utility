7ebf030d7e6b3232dc94a5159a5515c60e7c3dff
Jerry W <jerrywiltse@gmail.com>
2017-08-20 00:43:56 -0400

    enabled short paths

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ from conans import ConanFile
 class BoostUtilityConan(ConanFile):
     name = "Boost.Utility"
     version = "1.64.0"
+    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-utility"
     source_url = "https://github.com/boostorg/utility"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
