a47a4429cd7351e56f094445485a3f036cf74059
Rene Rivera <grafikrobot@gmail.com>
2017-12-06 08:13:50 -0600

    Require generator.

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,9 @@ from conans import ConanFile, tools
 class BoostUtilityConan(ConanFile):
     name = "Boost.Utility"
     version = "1.65.1"
-    requires = "Boost.Level5Group/1.65.1@bincrafters/testing"
+    requires = \
+        "Boost.Generator/1.65.1@bincrafters/testing", \
+        "Boost.Level5Group/1.65.1@bincrafters/testing"
     lib_short_names = ["utility"]
     is_header_only = True
     is_in_cycle_group = True
@@ -12,13 +14,10 @@ class BoostUtilityConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-utility"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
     license = "www.boost.org/users/license.html"
     short_paths = True
     build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
-
-    def package_id(self):
-        self.info.header_only()
 
     @property
     def env(self):
@@ -29,5 +28,8 @@ class BoostUtilityConan(ConanFile):
         except:
             pass
         return super(self.__class__, self).env
+
+    def package_id(self):
+        self.info.header_only()
 
     # END
