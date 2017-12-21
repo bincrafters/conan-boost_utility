60a474ceb8ca22c2dbc4d4c3b34d9ba31d9ef5d7
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:36 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,9 +3,9 @@ from conans import ConanFile
 
 class BoostUtilityConan(ConanFile):
     name = "Boost.Utility"
-    version = "1.65.1"
+    version = "1.66.0"
     requires = \
-        "Boost.Level5Group/1.65.1@bincrafters/testing"
+        "Boost.Level5Group/1.66.0@bincrafters/testing"
     lib_short_names = ["utility"]
     is_header_only = True
     is_in_cycle_group = True
@@ -13,9 +13,9 @@ class BoostUtilityConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-utility"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     exports = "boostgenerator.py"
 
