a0b1915835818b0a10373f82e40f5f53a4ec9087
Rene Rivera <grafikrobot@gmail.com>
2018-01-28 20:42:48 -0600

    Stable=>testing [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -18,8 +18,8 @@ class BoostUtilityConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/stable",
-        "boost_level5group/1.66.0@bincrafters/stable"
+        "boost_package_tools/1.66.0@bincrafters/testing",
+        "boost_level5group/1.66.0@bincrafters/testing"
     )
 
     # BEGIN
@@ -27,7 +27,7 @@ class BoostUtilityConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.66.0@bincrafters/stable"
+    build_requires = "boost_generator/1.66.0@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
